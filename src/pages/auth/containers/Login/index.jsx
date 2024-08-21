be9338639e9cd54e907093e6ab2540b4ae67f620
leavelet <leavelet@163.com>
2024-08-21 23:03:20 +0800

    Revert "feat: update default"
    This reverts commit 1d8c3e003e3ad80c3ab4d3581cac1caf2daaa833.

--- src/pages/auth/containers/Login/index.jsx
+++ src/pages/auth/containers/Login/index.jsx
@@ -262,13 +262,11 @@ export class Login extends Component {
           placeholder={t('Select a login type')}
           options={this.loginTypeOptions}
           onChange={this.onLoginTypeChange}
-          defaultValue={this.enableSSO ? this.SSOOptions[0].value : 'password'}
         />
       ),
     };
     if (this.enableSSO) {
       if (loginType === 'password') {
-        console.log('loginType:', loginType);
         return [typeItem, ...namePasswordItems, submitItem];
       }
 
